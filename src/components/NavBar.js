7b72c4aa7e16b70e3ae43b2a5fae63980ff79e90
MattheusKasa <mattheus.kasa@hotmail.se>
2023-01-04 19:29:05 +0000

    Add custom css, Styles directory

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,53 +5,52 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
     <div>
-         <Navbar bg="light" expand="md" fixed="top">
-      <Container fluid>
-        <Navbar.Brand href="#">Hobbyz</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="ml-auto text-left"
-          >
-            <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-          </Nav>
-          <Nav className="ml-auto text-left">
-            <Nav.Link href="#"><i className="fas fa-home"></i>Home</Nav.Link>
-            
-            <Nav.Link href="#">
-            <i className="fas fa-sign-in-alt"></i>Sign In
-            </Nav.Link>
-            <Nav.Link href="#">
-            <i className="fas fa-user-plus"></i>Sign Up
-            </Nav.Link>
-            
-            <NavDropdown title="Username" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Profile</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Sign Out
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here??
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
+            <Container fluid>
+              <Navbar.Brand href="#">Hobbyz</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  
+                >
+                  <Form className="d-flex">
+                    <Form.Control
+                      type="search"
+                      placeholder="Search for Hobbyz!"
+                      className="me-2"
+                      aria-label="Search"
+                    />
+                  <Button variant="outline-success">Search</Button>
+                  </Form>
+                </Nav>
+                <Nav>
+                  <Nav.Link href="#"><i className="fas fa-home"></i>Home</Nav.Link>
+                  <Nav.Link href="#">
+                    <i className="fas fa-sign-in-alt"></i>Sign In
+                  </Nav.Link>
+                  <Nav.Link href="#">
+                    <i className="fas fa-user-plus"></i>Sign Up
+                  </Nav.Link>
+                  
+                  <NavDropdown title="Username" id="navbarScrollingDropdown">
+                    <NavDropdown.Item href="#action3">Profile</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Sign Out
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here??
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+        </Navbar>
     </div>
   )
 }
