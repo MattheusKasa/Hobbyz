2e3bb2d0a9882e2e78d8a341ba33c651369eec35
MattheusKasa <mattheus.kasa@hotmail.se>
2023-01-05 14:02:40 +0000

    Add and import container to app.js, add styling to app.module.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { Button } from 'react-bootstrap';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
 
 
 function App() {
   return (
-    <div className="{styles.App}">
+    <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <h1>Home Page</h1>
+        <h1>Sign In</h1>
+      </Container>
     </div>
   );
 }
