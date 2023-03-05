ba8d719238ba53bf18c1690d16c28823620e42ae
MattheusKasa <mattheus.kasa@hotmail.se>
2023-03-05 15:28:37 +0000

    Add additional links to navbar including signout option, also add avatar.module.css for styling profile image

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,14 +20,29 @@ const NavBar = () => {
   <i className="far fa-plus-square"></i>Add Post
 </NavLink>
   )
-  const loggedInIcons = <>{currentUser?.username}</>
-  const loggedOutIcons = <> <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
-  <i className="fas fa-sign-in-alt"></i>Sign in
-</NavLink>
-<NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
-  <i className="fas fa-user-plus"></i>Sign Up
-</NavLink>
-</>
+  const loggedInIcons = <>
+    <NavLink 
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/feed">
+      <i className="fas fa-stream"></i>Feed
+    </NavLink>
+    <NavLink 
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/liked">
+      <i className="fas fa-heart"></i>liked
+    </NavLink>
+    
+  </>
+  const loggedOutIcons = <> 
+    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
+      <i className="fas fa-sign-in-alt"></i>Sign in
+    </NavLink>
+    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+      <i className="fas fa-user-plus"></i>Sign Up
+    </NavLink>
+  </>
 
   return (
     <div>
@@ -51,19 +66,30 @@ const NavBar = () => {
                   </Form>
                 </Nav>
                 <Nav>
-                  <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/"><i className="fas fa-home"></i>Home
-                  </NavLink>
                   
                   
                   
                   <NavDropdown title={currentUser ? loggedInIcons : loggedOutIcons} id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="#action3">Profile</NavDropdown.Item>
+                    <NavDropdown.Item>
+                    <NavLink 
+                      className={styles.NavLink}
+                      to="{`/profiles/${currentUser?.profile_id}`}"
+                      >
+                      <img src={currentUser?.profile_image} />
+                    </NavLink>
+                    </NavDropdown.Item>
                     <NavDropdown.Item href="#action4">
                       Hobbyz
                     </NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Sign Out
+                    <NavDropdown.Item >
+                    <NavLink 
+      className={styles.NavLink}
+      to="/"
+      onClick={() => {}}
+      >
+      <i className="fas fa-sign-out-alt"></i>Sign Out
+    </NavLink>
                     </NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
