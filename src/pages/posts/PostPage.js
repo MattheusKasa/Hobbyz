f7eb382b0a0bc302fe5c0ca53983b234463c9c5f
MattheusKasa <mattheus.kasa@hotmail.se>
2023-03-09 20:40:16 +0000

    Add like and comment icons to post.js

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
   const { id } = useParams();
@@ -31,14 +32,14 @@ function PostPage() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
-        <p>Post component</p>
+        <p>mobile</p>
+        <Post {...post.results[0]} setPost={setPost} PostPage />
         <Container className={appStyles.Content}>
           Comments
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        desktop
       </Col>
     </Row>
   );
