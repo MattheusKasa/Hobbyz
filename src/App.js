686a0c1b08978218a52940e8e6c4cf6b834e3e5a
MattheusKasa <mattheus.kasa@hotmail.se>
2023-03-10 01:02:50 +0000

    Add route paths for feed and liked page with added messages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
           <NavBar />
           <Container className={styles.Main}>
             <Switch>
-              <Route exact path="/" render={() => <PostsPage /> } />
+              <Route exact path="/" render={() => ( <PostsPage message="No results found. Try searching for another Hobby!" /> )} />
+              <Route exact path="/feed" render={() => ( <PostsPage message="No results found. Try following a user or searching for another Hobby!" filter={`owner__followed__owner__profile=${profile_id}&`} /> )} />
+              <Route exact path="/liked" render={() => ( <PostsPage message="No results found. Try liking a post or searching for another Hobby!" filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} /> )} />
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/posts/create" render={() => <PostCreateForm />} />
