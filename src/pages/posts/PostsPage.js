7da418737788d4d4b5b690f9926be6b2f9bed179
MattheusKasa <mattheus.kasa@hotmail.se>
2023-03-10 03:08:51 +0000

    Remove font awesome search icon from PostsPage

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -37,8 +37,8 @@ function PostsPage({ message, filter = "" }) {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>mobile</p>
-        <i className={`fas fa-search ${styles.SearchIcon}`} />
-        <Form 
+        <Form
+            
             classname={styles.SearchBar}
             onSubmit={(event) => event.preventDefault()}>
             <Form.Control
