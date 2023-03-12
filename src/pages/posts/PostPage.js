f916e19e614509d3cfcb724fe3d3006f0d87df74
MattheusKasa <mattheus.kasa@hotmail.se>
2023-03-12 00:23:44 +0000

    Fix bug where dropdown edit menu didnt show up, fixed by changing PostPage to postpage in PostPage.js

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
-
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
@@ -16,12 +14,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
         try {
-            const [{data: post}] = await Promise.all([
+            const [{ data: post }] = await Promise.all([
                 axiosReq.get(`/posts/${id}`)
-            ])
-            setPost({results: [post]})
-            console.log(post)
-        } catch(err){
+            ]);
+            setPost({ results: [post] });
+            console.log(post);
+        } catch (err) {
             console.log(err)
         }
     }
@@ -33,7 +31,7 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>mobile</p>
-        <Post {...post.results[0]} setPost={setPost} PostPage />
+        <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
           Comments
         </Container>
